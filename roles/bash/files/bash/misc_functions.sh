6534f82a8279bcc910346bd8665c7fc41bd5f0af
AlejandroSuero <asuerome@gmail.com>
2024-03-08 19:47:07 +0100

    trying _cmd way

--- roles/bash/files/bash/misc_functions.sh
+++ roles/bash/files/bash/misc_functions.sh
@@ -3,7 +3,8 @@
 function junk {
   for item in $@; do
     _task "Moving '$item' to the trash"
-    mv "$item" "$HOME/.Trash/"
-    _task_done
+    # mv "$item" "$HOME/.Trash/"
+    # _task_done
+    _cmd "mv $item $HOME/.Trash/"
   done;
 }
