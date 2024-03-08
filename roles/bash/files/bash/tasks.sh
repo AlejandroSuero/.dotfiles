cbe772412b66906f6b61761971862e4a62aeb7bc
AlejandroSuero <asuerome@gmail.com>
2024-03-08 19:06:10 +0100

    fix: /tmp location for logs

--- roles/bash/files/bash/tasks.sh
+++ roles/bash/files/bash/tasks.sh
@@ -6,7 +6,7 @@
 generate_log() {
     TIMESTAMP=$(date +%Y%m%d_%H%M%S)
     RANDOM_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-    echo "$TMP/bash._task.$TIMESTAMP-$RANDOM_STRING.log"
+    echo "/tmp/bash._task.$TIMESTAMP-$RANDOM_STRING.log"
 }
 
 
