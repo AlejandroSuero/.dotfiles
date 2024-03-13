bd65fea2776429bc59329be036ee959d890533ca
AlejandroSuero <asuerome@gmail.com>
2024-03-13 17:25:44 +0100

    feat: added get and open default browser in MacOSX

--- roles/bash/files/os/MacOSX/os_functions.sh
+++ roles/bash/files/os/MacOSX/os_functions.sh
@@ -11,5 +11,12 @@ _paste() {
   pbpaste
 }
 
+_get_default_browser() {
+  x=~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist
+  plutil -convert xml1 $x
+  grep 'https' -b3 $x | awk 'NR==2 {split($2, arr, "[><]"); print arr[3]}'
+  plutil -convert binary1 $x
+}
+
 alias c="_copy"
 alias v="_paste"
