4fca27e4ccc0f8b3ccc129811c2d33d976713caf
AlejandroSuero <asuerome@gmail.com>
2024-03-14 10:04:57 +0100

    feat: added exa aliases

---
+++ roles/bash/files/bash/exa_aliases.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+alias ls=$(exa --icons --classify)
+alias ll=$(exa -l --icons --classify)
+alias la=$(exa -la --icons --classify)
+alias lg=$(exa -la --git --icons --classify)
+alias lt=$(exa -la --tree --icons --classify)
