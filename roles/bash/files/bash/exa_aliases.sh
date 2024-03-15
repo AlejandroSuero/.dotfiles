b681fb8c8bb88b5c451b6b95800536cdfe0d86f2
AlejandroSuero <asuerome@gmail.com>
2024-03-15 19:17:18 +0100

    fix: aliases as not commands

--- roles/bash/files/bash/exa_aliases.sh
+++ roles/bash/files/bash/exa_aliases.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-alias ls=$(exa --icons --classify)
-alias ll=$(exa -l --icons --classify)
-alias la=$(exa -la --icons --classify)
-alias lg=$(exa -la --git --icons --classify)
-alias lt=$(exa -la --tree --icons --classify)
+alias ls="exa --icons --classify"
+alias ll="exa -l --icons"
+alias la="exa -la --icons --classify"
