bb3520b762413d628c0e136ab07dc4be429fc03e
AlejandroSuero <asuerome@gmail.com>
2024-03-27 20:18:40 +0100

    feat(neovim): adjusted `noice.nvim` options

--- roles/neovim/files/lua/custom/plugins/ui/noice.lua
+++ roles/neovim/files/lua/custom/plugins/ui/noice.lua
@@ -85,11 +85,12 @@ return {
         cmdline_popup = {
           border = {
             style = "none",
-            padding = { 2, 3 },
+            padding = { 1, 2 },
           },
           filter_options = {},
           win_options = {
             winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+            winblend = 0,
           },
         },
       },
