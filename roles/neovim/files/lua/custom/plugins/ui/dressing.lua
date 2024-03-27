32c6735d31c2df700940fe3410ad9adaa1273ab7
AlejandroSuero <asuerome@gmail.com>
2024-03-27 20:12:41 +0100

    feat(neovim): improved `dressing.nvim`

--- roles/neovim/files/lua/custom/plugins/ui/dressing.lua
+++ roles/neovim/files/lua/custom/plugins/ui/dressing.lua
@@ -4,6 +4,10 @@ return {
   opts = {
     input = {
       relative = "win",
+      title_pos = "center",
+      win_options = {
+        winblend = 0,
+      },
     },
     select = {
       backend = { "telescope", "builtin", "fzf_lua", "fzf", "nui" },
