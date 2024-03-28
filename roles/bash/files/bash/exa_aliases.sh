6e97bcafb546503500a8367f67c1a008f57d3e10
AlejandroSuero <asuerome@gmail.com>
2024-03-28 12:19:32 +0100

    feat(roles): removed kitty, winget and bash

--- roles/bash/files/bash/exa_aliases.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-alias ls="exa --icons --classify"
-alias ll="exa -l --icons"
-alias la="exa -la --icons --classify"
--- roles/bash/files/bash/keybinds.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bind -x '"\C-f":tmux-sessionizer'
-bind -x '"\C-g":lazygit'
-bind -x '"\C-b":$(_open_default_browser)'
