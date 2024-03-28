6c2c767f978d9f076cbbd893456da7e0f49c0802
AlejandroSuero <asuerome@gmail.com>
2024-03-28 16:39:35 +0100

    feat(wezterm): applied config

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -1,5 +1,7 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
+
+local action = wezterm.action
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,7 +9,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -19,26 +21,102 @@ end
 config.colors = {}
 config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font_with_fallback({
-	"JetBrainsMono Nerd Font Mono",
-	"Berkeley Mono",
-	"nonicons",
-})
+config.font = wezterm.font_with_fallback {
+  {
+    family = "JetBrainsMono Nerd Font Mono",
+    weight = 600,
+    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  },
+  "Berkeley Mono",
+  "nonicons",
+}
+
+config.font_size = 16
 
 -- default is true, has more "native" look
 config.use_fancy_tab_bar = false
 
 -- I don't like putting anything at the ege if I can help it.
 config.enable_scroll_bar = false
+
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 5,
+  right = 5,
+  top = 5,
+  bottom = 5,
 }
 
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+
+config.inactive_pane_hsb = {
+  saturation = 0.8,
+  brightness = 0.7,
+}
+
+config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.freetype_load_target = "HorizontalLcd"
+
+config.leader = { key = " ", mods = "SHIFT", timeout_milliseconds = 2000 }
+config.keys = {
+  {
+    key = "c",
+    mods = "LEADER",
+    action = action.SpawnTab "CurrentPaneDomain",
+  },
+  {
+    key = "J",
+    mods = "LEADER",
+    action = action.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "L",
+    mods = "LEADER",
+    action = action.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "l",
+    mods = "CTRL",
+    action = action.ActivatePaneDirection "Right",
+  },
+  {
+    key = "j",
+    mods = "CTRL",
+    action = action.ActivatePaneDirection "Down",
+  },
+  {
+    key = "k",
+    mods = "CTRL",
+    action = action.ActivatePaneDirection "Up",
+  },
+  {
+    key = "h",
+    mods = "CTRL",
+    action = action.ActivatePaneDirection "Left",
+  },
+  {
+    key = "r",
+    mods = "LEADER",
+    action = action.ReloadConfiguration,
+  },
+  {
+    key = "R",
+    mods = "LEADER",
+    action = action.PromptInputLine {
+      description = "Enter new name for tab",
+      action = wezterm.action_callback(function(window, _, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+}
 
 -- and finally, return the configuration to wezterm
 return config
