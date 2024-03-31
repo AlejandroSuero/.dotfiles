e869f61acff1fa07521f0688fbaf24bf4ef3c392
AlejandroSuero <asuerome@gmail.com>
2024-03-31 16:56:15 +0200

    feat(aliases): added node and package manager aliases

---
+++ roles/zsh/files/zsh/node_aliases.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+choose_pkg_mgr() {
+  packages=("npm" "pnpm" "bun")
+  choice=$(printf "%s\n" "${packages[@]}" | fzf)
+  echo "$choice"
+}
+
+# p for package
+alias p="choose_pkg_mgr"
+
+# p + cmd
+alias pi="choose_pkg_mgr install"
+alias pie="choose_pkg_mgr install -E"
+alias pidev="choose_pkg_mgr install -D -E"
+
+alias pdev="choose_pkg_mgr dev"
+alias pstart="choose_pkg_mgr start"
+alias pbuild="choose_pkg_mgr build"
+
+alias plint="choose_pkg_mgr lint"
+alias pformat="choose_pkg_mgr format"
+
+# vi:ft=zsh
