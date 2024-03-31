e10ba945af2b699abb1af087715bca4512ff9bd5
AlejandroSuero <asuerome@gmail.com>
2024-03-31 17:04:50 +0200

    fix(aliases): use choice value

--- roles/zsh/files/zsh/node_aliases.sh
+++ roles/zsh/files/zsh/node_aliases.sh
@@ -7,18 +7,18 @@ choose_pkg_mgr() {
 }
 
 # p for package
-alias p="choose_pkg_mgr"
+alias p="$(choose_pkg_mgr)"
 
 # p + cmd
-alias pi="choose_pkg_mgr install"
-alias pie="choose_pkg_mgr install -E"
-alias pidev="choose_pkg_mgr install -D -E"
+alias pi="$(choose_pkg_mgr) install"
+alias pie="$(choose_pkg_mgr) install -E"
+alias pidev="$(choose_pkg_mgr) install -D -E"
 
-alias pdev="choose_pkg_mgr dev"
-alias pstart="choose_pkg_mgr start"
-alias pbuild="choose_pkg_mgr build"
+alias pdev="$(choose_pkg_mgr) dev"
+alias pstart="$(choose_pkg_mgr) start"
+alias pbuild="$(choose_pkg_mgr) build"
 
-alias plint="choose_pkg_mgr lint"
-alias pformat="choose_pkg_mgr format"
+alias plint="$(choose_pkg_mgr) lint"
+alias pformat="$(choose_pkg_mgr) format"
 
 # vi:ft=zsh
