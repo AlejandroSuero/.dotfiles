5c3cbb0360a49129a57f88caad540076cb43c509
AlejandroSuero <asuerome@gmail.com>
2024-04-04 00:22:04 +0200

    feat(neovim): removed unused colorschemes

--- roles/neovim/files/lua/aome/colorschemes/poimandres.lua
+++
@@ -1,7 +0,0 @@
-return {
-  bold_vert_split = false, -- use bold vertical separators
-  dim_nc_background = true, -- dim 'non-current' window backgrounds
-  disable_background = vim.g.transparency, -- disable background
-  disable_float_background = false, -- disable background for floats
-  disable_italics = true, -- disable italics
-}
--- roles/neovim/files/lua/custom/plugins/ui/colorschemes.lua
+++ roles/neovim/files/lua/custom/plugins/ui/colorschemes.lua
@@ -1,37 +1,6 @@
 local configs = "aome.colorschemes."
 return {
   {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = require(configs .. "kanagawa"),
-  },
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-    name = "catppuccin",
-    opts = require(configs .. "catppuccin"),
-  },
-  {
-    "neanias/everforest-nvim",
-    lazy = false,
-    priority = 1000,
-    opts = require(configs .. "everforest"),
-    config = function(_, opts)
-      require("everforest").setup(opts)
-    end,
-  },
-  {
-    "olivercederborg/poimandres.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = require(configs .. "poimandres"),
-    config = function(_, opts)
-      require("poimandres").setup(opts)
-    end,
-  },
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
