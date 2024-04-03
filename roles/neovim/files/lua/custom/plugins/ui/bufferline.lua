eff42b66b0a3b1488277095b76fc8bc0c083c3ce
AlejandroSuero <asuerome@gmail.com>
2024-04-04 00:22:26 +0200

    feat(neovim): added bufferline for tabs

---
+++ roles/neovim/files/lua/custom/plugins/ui/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  event = { "VeryLazy" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "tabs",
+      separator_style = "thin",
+    },
+  },
+}
