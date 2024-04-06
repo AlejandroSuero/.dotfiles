dc72fbeb966330165a768199e6a4a2e4f4de3159
AlejandroSuero <asuerome@gmail.com>
2024-04-06 13:13:07 +0200

    feat(lua): added luarocks support in `MacOSX`

--- roles/zsh/files/zsh/path_vars.sh
+++ roles/zsh/files/zsh/path_vars.sh
@@ -5,6 +5,7 @@ addToPath $GOPATH/bin
 addToPath $HOME/go/bin
 addToPath $HOME/.dotfiles/bin
 addToPath $HOME/.cargo/bin
+addToPath $HOME/.luarocks/bin
 addToPathFront $HOME/.local/bin
 # ccache
 addToPathFront /usr/lib/ccache
