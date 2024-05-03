1c2eb380d79f3bd734e101ae28c66fa4f9c7a526
AlejandroSuero <asuerome@gmail.com>
2024-05-03 16:48:46 +0200

    feat(neovim): added `freeze` keymaps

--- roles/neovim/files/lua/custom/plugins/ui/freeze.lua
+++ roles/neovim/files/lua/custom/plugins/ui/freeze.lua
@@ -2,5 +2,13 @@ return {
   "AlejandroSuero/freeze.nvim",
   branch = "feature/add-installation",
   config = true,
-  cmd = { "Freeze", "FreezeLine" },
+  keys = {
+    { "<leader>fz", "<cmd>Freeze<cr>", desc = "[f]ree[z]e file" },
+    {
+      "<leader>fz",
+      "<cmd>FreezeLine<cr>",
+      desc = "[f]ree[z]e line",
+      mode = "v",
+    },
+  },
 }
