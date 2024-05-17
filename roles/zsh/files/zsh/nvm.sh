3b66c729b600821b3c79d61aa6c93ef9895357f2
AlejandroSuero <asuerome@gmail.com>
2024-05-17 12:43:46 +0200

    feat(zsh): nvm not found warn

--- roles/zsh/files/zsh/nvm.sh
+++ roles/zsh/files/zsh/nvm.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env zsh
 
 check_nvmrc() {
-  if [[ -f "$PWD"/.nvmrc ]]; then
-    nvm use > /dev/null
+  if [[ "$(command -v nvm)" == "" ]]; then
+    echo -e "${WARNING} ${YELLOW}nvm not found${NC}"
+  else
+    if [[ -f "$PWD"/.nvmrc ]]; then
+      nvm use > /dev/null
+    fi
   fi
 }
 
